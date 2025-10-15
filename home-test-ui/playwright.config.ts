64c838579341f907c3cd33c89de15e059a03ee56
Your Name <you@example.com>
2025-10-15 13:00:17 -0300

    Docker files

--- home-test-ui/playwright.config.ts
+++ home-test-ui/playwright.config.ts
@@ -25,9 +25,9 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://localhost:3100',
+    baseURL: process.env.BASE_URL || 'http://localhost:3100',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
